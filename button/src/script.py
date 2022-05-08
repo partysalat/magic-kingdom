103bd5aedae2c9139b1634fdc8f7668672b9b9cd
Ben Lenser <ben.lenser@immobilienscout24.de>
2022-05-08 13:20:30 +0200

    Search files for filename

--- button/src/script.py
+++ button/src/script.py
@@ -1,43 +1,44 @@
 from gpiozero import LED, Button
-from signal import pause
 import random
 from playsound import playsound
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+from os import listdir
+from os.path import isfile, join
 
-
-sound_files = [
-    "human1.mp3",
-    "human2.mp3",
-    "human3.mp3",
-    "human4.mp3",
-    "human5.mp3",
-    "human6.mp3",
-    "human7.mp3",
-    "human8.mp3",
-    "human9.mp3",
-    "human10.mp3",
-    "human11.mp3",
-    "human12.mp3",
-    "human13.mp3",
-    "human14.mp3",
-    "human15.mp3",
-    "human16.mp3",
-    "human17.mp3",
-    # "human18.mp3",
-    "human19.mp3",
-    "orcs1.mp3",
-    "orcs2.mp3",
-    "orcs3.mp3",
-    "orcs4.mp3",
-    "orcs5.mp3",
-    "orcs6.mp3",
-    "orcs7.mp3",
-    "orcs8.mp3",
-    "orcs9.mp3",
-    "orcs10.mp3",
-    "orcs11.mp3",
-]
+sound_files_path = "/home/pi/brabutton/sounds/"
+sound_files = [f for f in listdir(sound_files_path) if isfile(join(sound_files_path, f))]
+# sound_files = [
+#     "human1.mp3",
+#     "human2.mp3",
+#     "human3.mp3",
+#     "human4.mp3",
+#     "human5.mp3",
+#     "human6.mp3",
+#     "human7.mp3",
+#     "human8.mp3",
+#     "human9.mp3",
+#     "human10.mp3",
+#     "human11.mp3",
+#     "human12.mp3",
+#     "human13.mp3",
+#     "human14.mp3",
+#     "human15.mp3",
+#     "human16.mp3",
+#     "human17.mp3",
+#     # "human18.mp3",
+#     "human19.mp3",
+#     "orcs1.mp3",
+#     "orcs2.mp3",
+#     "orcs3.mp3",
+#     "orcs4.mp3",
+#     "orcs5.mp3",
+#     "orcs6.mp3",
+#     "orcs7.mp3",
+#     "orcs8.mp3",
+#     "orcs9.mp3",
+#     "orcs10.mp3",
+#     "orcs11.mp3",
+# ]
 led = LED(25)
 button = Button(23)
 
