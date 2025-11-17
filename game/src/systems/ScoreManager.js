c1d52878adf6566e8244c32af880d1a77130db19
Ben Lenser <lenser.ben@gmail.com>
2025-11-17 11:07:14 +0100

    feat: add boss victory bonuses to ScoreManager
    - Add boss-specific bonus point values
    - Implement addBossVictory method
    - Iron Shell: 500 points
    - Kraken's Arm: 750 points
    - Leviathan: 1000 points

--- game/src/systems/ScoreManager.js
+++ game/src/systems/ScoreManager.js
@@ -7,6 +7,13 @@ export class ScoreManager {
         this.ENEMY_KILL_POINTS = 10;
         this.WAVE_SURVIVAL_BONUS = 50;
 
+        // Boss victory bonuses
+        this.BOSS_VICTORY_BONUS = {
+            boss_iron_shell: 500,
+            boss_kraken_arm: 750,
+            boss_leviathan: 1000
+        };
+
         console.log('ScoreManager initialized');
     }
 
@@ -26,6 +33,13 @@ export class ScoreManager {
         this.score += bountyValue;
         console.log('Bounty killed! Bonus:', bountyValue, 'Score:', this.score);
         return this.score;
+    }
+
+    addBossVictory(bossType) {
+        const bonus = this.BOSS_VICTORY_BONUS[bossType] || 500;
+        this.score += bonus;
+        console.log('Boss defeated! Bonus:', bonus, 'Score:', this.score);
+        return { score: this.score, bonus: bonus };
     }
 
     getScore() {
